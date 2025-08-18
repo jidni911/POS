7365b0a28c0ee03f34d441e4e0b6e9c6c4467784
jidni911 <gidni441@gmail.com>
2025-08-19 00:15:52 +0600

    .

--- src/main/java/com/zab/pos/entity/XRolePrivilege.java
+++ src/main/java/com/zab/pos/entity/XRolePrivilege.java
@@ -2,8 +2,7 @@ package com.zab.pos.entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.IdClass;
